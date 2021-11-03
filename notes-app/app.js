81c4d196e282e0a30e9a5d686a44c3423a856537
Blanco, Ramiro <rablanco@netsuite.com>
2021-11-03 08:41:21 +0100

    adds yargs commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,47 @@
 const getNotes = require('./notes')
+const yargs = require('yargs')
 const chalk = require('chalk')
 
-const msg = getNotes()
-console.log(msg)
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.bold.green('Success!'))
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Lists all your notes',
+    handler: function() {
+        console.log('Listing all your notes!')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads the note you pass as an option',
+    handler: function() {
+        console.log('Here is your note, ready for reading!')
+    }
+})
+
+//add, remove, read, list
+
+
+console.log(yargs.argv)
