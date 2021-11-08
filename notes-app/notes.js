f0da5ac7438925138e22a67e3f9f7f43569000dc
Blanco, Ramiro <rablanco@netsuite.com>
2021-11-08 09:06:30 +0100

    removes debugger

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -15,6 +15,7 @@ const read = (title) => {
 const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNote = notes.find(note => note.title === title)
+debugger
 
     if(!duplicateNote) {
         notes.push({
