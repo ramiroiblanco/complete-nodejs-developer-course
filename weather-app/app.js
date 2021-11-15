502a1d4f885453f945b1b4e8cf9b6400604359d5
Blanco, Ramiro <rablanco@netsuite.com>
2021-11-15 09:02:25 +0100

    callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,28 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=11e4a55295d92629e242555e9faac2f6&query=Barcelona'
+// const url = 'http://api.weatherstack.com/current?access_key=11e4a55295d92629e242555e9faac2f6&query=Barcelona'
+//
+// request({url: url, json: true}, async (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to weather service!')
+//     } else if( response.body.error) {
+//         console.log('Unable to find location.')
+//     } else {
+//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+//     }
+// })
 
-request({url: url, json: true}, async (error, response) => {
-    if(error){
-        console.log('Unable to connect to weather service!')
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/barcelona.json?access_token=pk.eyJ1IjoicmFtaXJvaWJsYW5jbyIsImEiOiJja3Z3MTc5cDljMXJlMzBzN3BrZ2ZuOWp3In0.Cfiro0uITanrKpRTj6J6tg'
+
+request({url: geocodeURL, json: true}, async (error, response) => {
+    if(error) {
+        console.log('Unable to connect to geolocation services!')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
     } else {
-        console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+        const latitude = response.body.features[0].center[0]
+        const longitude = response.body.features[0].center[1]
+
+        console.log(latitude, longitude)
     }
 })
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/barcelona.json?access_token=pk.eyJ1IjoicmFtaXJvaWJsYW5jbyIsImEiOiJja3Z3MTc5cDljMXJlMzBzN3BrZ2ZuOWp3In0.Cfiro0uITanrKpRTj6J6tg'
-//
-// request({url: geocodeURL, json: true}, async (error, response) => {
-//     const latitude = response.body.features[0].center[0]
-//     const longitude = response.body.features[0].center[1]
-//
-//     console.log(latitude, longitude)
-// })
