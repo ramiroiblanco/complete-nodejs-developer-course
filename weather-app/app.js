070607b83e8c7bb1e295b1e66723b699025f3506
Blanco, Ramiro <rablanco@netsuite.com>
2021-11-15 08:09:15 +0100

    starts weatherapp

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,20 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=11e4a55295d92629e242555e9faac2f6&query=Barcelona&units=f'
+const url = 'http://api.weatherstack.com/current?access_key=11e4a55295d92629e242555e9faac2f6&query=Barcelona'
 
 request({url: url, json: true}, async (error, response) => {
-    // console.log(`this is error ${error}`)
-    // console.log(`this is response`, response.body)
-    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+    if(error){
+        console.log('Unable to connect to weather service!')
+    } else {
+        console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+    }
 })
+
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/barcelona.json?access_token=pk.eyJ1IjoicmFtaXJvaWJsYW5jbyIsImEiOiJja3Z3MTc5cDljMXJlMzBzN3BrZ2ZuOWp3In0.Cfiro0uITanrKpRTj6J6tg'
+//
+// request({url: geocodeURL, json: true}, async (error, response) => {
+//     const latitude = response.body.features[0].center[0]
+//     const longitude = response.body.features[0].center[1]
+//
+//     console.log(latitude, longitude)
+// })
