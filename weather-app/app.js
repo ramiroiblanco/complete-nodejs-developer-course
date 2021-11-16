f7c04fa8ba926525e303723a651a0a9f57b66e68
Blanco, Ramiro <rablanco@netsuite.com>
2021-11-16 08:51:26 +0100

    adds forecast and geocode to weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,28 +1,12 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'http://api.weatherstack.com/current?access_key=11e4a55295d92629e242555e9faac2f6&query=Barcelona'
-//
-// request({url: url, json: true}, async (error, response) => {
-//     if(error){
-//         console.log('Unable to connect to weather service!')
-//     } else if( response.body.error) {
-//         console.log('Unable to find location.')
-//     } else {
-//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
-//     }
-// })
+geocode(`carrer d'indibil, 12`, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/barcelona.json?access_token=pk.eyJ1IjoicmFtaXJvaWJsYW5jbyIsImEiOiJja3Z3MTc5cDljMXJlMzBzN3BrZ2ZuOWp3In0.Cfiro0uITanrKpRTj6J6tg'
-
-request({url: geocodeURL, json: true}, async (error, response) => {
-    if(error) {
-        console.log('Unable to connect to geolocation services!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[0]
-        const longitude = response.body.features[0].center[1]
-
-        console.log(latitude, longitude)
-    }
+forecast(41.369574, 2.143957, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
