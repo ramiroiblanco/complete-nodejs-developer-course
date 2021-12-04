665a297bf7e2b00f097c5a16af365b1305f8a3b6
Blanco, Ramiro [SET Pathfinder] <ramiro.blanco@oracle.com>
2021-12-04 10:35:58 +0100

    chains geocode and forecast

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode(`carrer d'indibil, 12`, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
 
-forecast(41.369574, 2.143957, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if(!address){
+    console.log('Please provide an address.')
+} else {
+    geocode(address, (error, data) => {
+        if(error) {
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
+
