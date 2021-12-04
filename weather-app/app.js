9e86c1e005a4a0da25406ab5c4b036e382474146
Blanco, Ramiro [SET Pathfinder] <ramiro.blanco@oracle.com>
2021-12-04 10:46:21 +0100

    test

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,7 @@ const forecast = require('./utils/forecast')
 const address = process.argv[2]
 
 if(!address){
-    console.log('Please provide an address.')
+    console.log('Provide an address.')
 } else {
     geocode(address, (error, data) => {
         if(error) {
