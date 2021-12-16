25c8cdcd3d0d7f78f0cdc2a73094a181648a2afd
Blanco, Ramiro [SET Pathfinder] <ramiro.blanco@oracle.com>
2021-12-16 08:09:24 +0100

    creates search form

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require("request");
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoicmFtaXJvaWJsYW5jbyIsImEiOiJja3Z3MTc5cDljMXJlMzBzN3BrZ2ZuOWp3In0.Cfiro0uITanrKpRTj6J6tg`
 
-    request({url, json: true}, (error, {body}) => {
+    request({url, json: true}, (error, {body} = {}) => {
         if(error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0){
